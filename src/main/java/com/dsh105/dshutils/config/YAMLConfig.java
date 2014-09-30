86c51c0a6f4fa017548681a9a8c5ee01c28b0543
DSH105 <jason.chirgwin@icloud.com>
2014-09-30 12:58:02 +1000

    Load configuration using UTF-8 charset
    Pulls changes from DSH105/Commodus@f7cf2f7 so that they are available
    in EchoPet

--- src/main/java/com/dsh105/dshutils/config/YAMLConfig.java
+++ src/main/java/com/dsh105/dshutils/config/YAMLConfig.java
@@ -7,6 +7,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Set;
 
@@ -23,7 +25,14 @@ public class YAMLConfig {
         this.manager = new YAMLConfigManager(plugin);
 
         this.file = configFile;
-        this.config = YamlConfiguration.loadConfiguration(configStream);
+        try {
+            this.config = YamlConfiguration.loadConfiguration(new InputStreamReader(configStream, "UTF-8"));
+        } catch (NoSuchMethodError ignored) {
+        } catch (UnsupportedEncodingException ignored) {
+        }
+        if (this.config == null) {
+            this.config = YamlConfiguration.loadConfiguration(configStream);
+        }
         this.plugin = plugin;
     }
 
